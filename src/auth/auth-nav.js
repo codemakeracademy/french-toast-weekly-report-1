f59bb5de030483e78ba408e0bdc18cdef4318881
1yoga <1yoga@mail.ru>
2021-12-14 23:32:27 +0700

    with auth 4

--- src/auth/auth-nav.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {AuthenticationButton} from "../modules/common/buttons/auth-buttons/authentication-button";
-
-export const AuthNav = () => (
-    <div className="navbar-nav ml-auto">
-        <AuthenticationButton />
-    </div>
-);
-
